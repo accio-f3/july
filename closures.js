8b469b790183a6ab323ba1b9469a29d77e671252
Prikshit <prikshit@Prikshits-MacBook-Air.local>
2023-07-23 12:46:03 +0530

    revision

---
+++ closures.js
@@ -0,0 +1,47 @@
+// // what is js?
+
+// // JS is Sync and Single Threaded language.
+
+// // Single Threaded -> it can excecute only 1
+// // command at a time
+
+// // sync single threaded -> J.S. can exceute 1 command at time
+// // and in a specific order
+
+
+// // callStack -> it handles everything related to Execution
+// // context.
+// // what is everything?
+// // creation/ deletion /code control
+
+// // function a(){
+// //     var b=10;
+// // }
+// // a();
+// // console.log(b);
+
+// function a(){
+//     // var b=10;
+//     c();
+//     console.log(b);
+//     function c(){
+//         var b=10;
+//         console.log(b);
+//         return b;
+//     }
+// }
+
+
+// a();
+// // var b=10;
+// console.log(b);
+
+// a is having exection context inside
+// code block of GEC
+
+// inside a's ec-> c's ec
+
+// scope of a variable -> where you can access a variable
+
+// scope is directly dependent on lexical env.
+// lexical -> as a term means in heirchy or in a sequence
