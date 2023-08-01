be8f16eb2d49e4e45964a045a3cfd9cc9426a34b
Prikshit <prikshit@Prikshits-MacBook-Air.local>
2023-08-01 20:14:41 +0530

    debouncing 2nd method

--- throttling.js
+++ throttling.js
@@ -16,18 +16,31 @@ const brokenShotGun = () =>{
     throttleCountDisplay.innerHTML=++throttleCount;
 }
 
-const magicFn = (callback,delay) =>{
-    // it will store a timer as 0 for the first fn call
-    let lastFuncExTime = 0;
-    return function(...args){
-        let currentTime = new Date().getTime();
-        if(currentTime-lastFuncExTime<delay) return;
-        else{
-            lastFuncExTime=currentTime;
-            return callback(...args);
-        }
-    }
-}
+// const magicFn = (callback,delay) =>{
+//     // it will store a timer as 0 for the first fn call
+//     let lastFuncExTime = 0;
+//     return function(...args){
+//         let currentTime = new Date().getTime();
+//         if(currentTime-lastFuncExTime<delay) return;
+//         else{
+//             lastFuncExTime=currentTime;
+//             return callback(...args);
+//         }
+//     }
+// }
+
+// const magicFn = (callback,delay) =>{
+//     let execute = true;
+//     return function(...args){
+//         if(execute){
+//             callback(...args);
+//             execute=false;
+//             setTimeout(()=>{
+//                 execute=true;
+//             },delay);
+//         }
+//     }
+// }
 
 const fixedShotGun = magicFn(brokenShotGun,700);
 
